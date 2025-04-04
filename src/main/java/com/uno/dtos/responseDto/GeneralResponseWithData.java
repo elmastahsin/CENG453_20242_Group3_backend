1dbbe008199c8cf640942885245767cb565b3ac7
Tahsin Elmas <elmas.tahsin@metu.edu.tr>
2025-04-04 15:12:02 +0300

    mvn lombok problems

--- src/main/java/com/uno/dtos/responseDto/GeneralResponseWithData.java
+++ src/main/java/com/uno/dtos/responseDto/GeneralResponseWithData.java
@@ -1,16 +1,17 @@
 package com.uno.dtos.responseDto;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 
-@Getter
-@Setter
-@AllArgsConstructor
+@Data
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class GeneralResponseWithData<T> {
     private Status status;
     private T data;
 }
+
